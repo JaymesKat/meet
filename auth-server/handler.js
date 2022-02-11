8d101acd4e71a7509912b9b28452976ee78127fe
Jessica Daye <jddaye@hotmail.com>
2022-02-11 18:26:41 -0500

    added correct uri path

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -12,7 +12,7 @@ const credentials = {
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: ["https://jddaye.github.io/meet"],
+  redirect_uris: ["https://jddaye.github.io/meet/"],
   javascript_origins: ["https://jddaye.github.io", "http://localhost:3000"],
 };
 
